b2ed4722b44c6bb1c9fc48ed5645687fc5fa53f9
MEA-23 <77838294+MEA-23@users.noreply.github.com>
2024-10-20 21:48:34 +0300

    no need to set it manually

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ let port = process.env.PORT;
 app.use(cors());
 app.use(express.json())
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+// not needed as we use cors
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+//   }); 
 
 app.use('/user',userRouter);
 app.use('/category',categoryRouter);
