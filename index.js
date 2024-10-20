65352e4acbfcd14c7cd0e66235543b2091eab36e
YasmeenRadwan <engyradwan93@gmail.com>
2024-10-20 20:09:37 +0200

    cors

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ let port = process.env.PORT;
 app.use(cors());
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 app.use('/user',userRouter);
 app.use('/category',categoryRouter);
 app.use('/menu',menuRouter);
